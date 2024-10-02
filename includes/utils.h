b51a09ca122ef47a519d5e5ef0fee7eeb1f6772a
Benjamin Grolleau <benjamin.grolleau@outlook.com>
2024-10-02 18:01:26 +0200

    Debug printing

--- includes/utils.h
+++ includes/utils.h
@@ -1,4 +1,19 @@
 #ifndef UTILS_H
-#define UTULS_H
+#define UTILS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifdef DEBUG
+  #define INFO(fmt, ...)  fprintf(stderr, "INFO: " fmt, __VA_ARGS__)
+  #define WARN(fmt, ...)  fprintf(stderr, "WARN: " fmt, __VA_ARGS__)
+  #define ERROR(fmt, ...) fprintf(stderr, "ERROR: " fmt, __VA_ARGS__)
+#else
+  #define INFO(fmt, ...)  do { } while (0)
+  #define WARN(fmt, ...)  do { } while (0)
+  #define ERROR(fmt, ...) do { } while (0)
+#endif
+
+void print_error(int result, char* s);
 
 #endif
